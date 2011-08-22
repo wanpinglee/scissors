84cc5e31987ee4eef1f2e9cbccf894b992b4051d
wanpinglee <wanping.lee@gmail.com>
2011-08-22 15:35:54 -0400

    Test input bam

--- src/main.cpp
+++ src/main.cpp
@@ -51,110 +51,13 @@ int main ( int argc, char** argv ) {
 	}
 
 
-/*
-	// check all the I/O
-	// By Wan-Ping
-	if ( !checkFiles( parameterparser ) ) {
-		cout << "ERROR: Errors in I/O are found." << endl;
-		return 1;
-	}
-
-	// By Jiantao
-	// store an alignment
-	BamAlignment anchorAlignment, targetAlignment;
-	
-	// By Jiantao
-	// this checker can be put in checkFiles
-	BamReader bamReader;
-	bamReader.open( parameterparser.inputBam.c_str() );
-	if ( !bamReader.isGood() ) {
-		cout << "ERROR: Cannot open input bam" << endl;
-		return 1;
-	}
-
-	// By Wan-Ping
-	// this checker can be put in checkFile
-	BamWriter bamWriter;
-	bamWriter.open( parameterparser.outputBam.c_str() );
-	if ( !bamWriter.isGood() ) {
-		cout << "ERROR: Cannot open output bam" << endl;
-		return 1;
-	}
-
-	Reference reference;
-	HashTable hashTable;
-	loadReference( reference );
-	loadHashTable( hashTable );
-
-	while ( loadPairAlignments( anchorAlignment, targetAlignment) ) {  // By Jiantao
-	
-		if ( reference.index < anchorAlignment.index ) {
-			loadReference( reference );
-			loadHashTable( hashTable );
-		}
-
-		// By Jiantao
-		HashRegion* hashRegions;
-		// get hash regions for targetAlignment
-		getHashRegion( reference, hashTable, anchorAlignment.referenceBegin, targetAlignment.bases, hashRegions );
-
-		// By Wan-Ping
-		vector< Alignment > alignments;
-		// interface between C and C++
-		pickHashRegion( hashRegions, alignments ); // pick hash regions and store them in alignments
-
-		// By Jiantao
-		free_HashRegion( hashRegions );
-
-		// By Wan-Ping
-		smithWatermanAlign( alignments, reference );
-
-		// By Wan-Ping
-		vecotr< Alignment > splitReadAlignments;
-		comfirmSplitAlignment( anchorAlignment, alignments, splitReadAlignments ); // refine alignments and decide the result
-		BamWriter( anchorAlignment, splitReadAlignments );
-	}
-
-
-	// free memory
-	// By Jiantao
-	free_BamAlignment( anchorAlignment );
-	free_BamAlignment( targetAlignment );
-	free_Reference( reference );
-	free_HashTable( hashTable );
-	bamReader.close();
-	// By Wan-Ping
-	bamWriter.close();
-
-	
-	// ================
-	// END of prototype
-	// ================
-*/
-
-
-/*
-	uint64_t referenceBufferLength = 1000000;
-	SR_Reference* pRef = SR_ReferenceAlloc( referenceBufferLength );
-
-	// open hash table
-	FILE* hashTableInput = openFile( parameterparser.inputReferenceHash + ".ref", "rb" );
-
-	unsigned short hashSize = SR_ReadHashSize( hashTableInput );
-
-	// create a hash region table object
-	// the parameter given here is the edge tolerance percentage which is not useful here
-	HashRegionTable* pRegionTable = HashRegionTableAlloc(0.25);
-	
-	
-	// close files
-	fclose( hashTableInput );
-*/
-
-
+	// free memory and close files
 	SR_QueryRegionFree( query_region );
 	SR_BamInStreamFree( bam_reader );
 	bam_writer.Close();
+
+	cout << "Program done." << endl;
+
 	return 0;
 
 }
