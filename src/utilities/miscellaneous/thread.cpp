758e95eabf53034bf16c6843d6165f64a1d439ce
wanpinglee <wanping.lee@gmail.com>
2012-01-30 11:10:33 -0500

    Prepare to merge the master

--- src/utilities/miscellaneous/thread.cpp
+++ src/utilities/miscellaneous/thread.cpp
@@ -45,12 +45,10 @@ void StoreAlignmentInBam(const vector<bam1_t*>& alignments_bam,
 }
 
 void FreeAlignmentBam(vector<bam1_t*>* als_bam) {
-  
   for (unsigned int i = 0; i < als_bam->size(); ++i) {
     bam1_t* ptr = (*als_bam)[i];
     bam_destroy1(ptr);
   }
-  
 
   als_bam->clear();
 }
