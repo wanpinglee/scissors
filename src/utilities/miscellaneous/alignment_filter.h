fda457a87fd370979b968e9d4391aabf91f44c7e
wanpinglee <wanping.lee@gmail.com>
2012-04-25 11:21:42 -0400

    Add some comments

--- src/utilities/miscellaneous/alignment_filter.h
+++ src/utilities/miscellaneous/alignment_filter.h
@@ -8,18 +8,23 @@
 // The filter is used for split-read (SR) alignments.
 struct AlignmentFilter {
   // the ratio of SR bases to orignal mate bases [0.0-1.0]
+  // default: 0.3
   float aligned_base_rate;
   // the ratio of mismatches to SR bases [0.0-1.0]
+  // default: 0.1
   float allowed_mismatch_rate;
   // the score of matches for TrimAlignment function
+  // default: 1
   int trimming_match_score;
   // the score of mismatches for TrimAlignment function
+  // default: -2
   int trimming_mismatch_score;
   // the score of gaps for TrimAlignment function
+  // default: -2
   int trimming_gap_score;
 
   AlignmentFilter()
-      : aligned_base_rate(0.1)
+      : aligned_base_rate(0.3)
       , allowed_mismatch_rate(0.1)
       , trimming_match_score(1)
       , trimming_mismatch_score(-2)
